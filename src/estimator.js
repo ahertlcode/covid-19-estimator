0ddbad21124ff4ba35bfd4ac9fe44f8bf6359f18
Apetu Abayomi <abayomismart@gmail.com>
2020-04-13 14:14:40 +0100

    completed challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,18 @@
-const covid19ImpactEstimator = (data) => data;
-
+const covid19ImpactEstimator = (data) => {
+    let impact = {};
+    let severeImpact = {};
+    let days, factor;
+    if(data.periodType == "days"){
+        days = data.timeToElapse;
+    }else if(data.periodType == "weeks"){
+        days = data.timeToElapse*7;
+    }else if(data.periodType == "months"){
+        days = data.timeToElapse*30;
+    }
+    factor = parseInt(days/3);
+    impact.currentlyInfected = data.reportedCases*10;
+    impact.infectionsByRequestedTime = impact.currentlyInfected*(Math.pow(2, factor));
+    severeImpact.currentlyInfected = data.reportedCases*50;
+    severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected*(Math.pow(2, factor));
+    }
 export default covid19ImpactEstimator;
